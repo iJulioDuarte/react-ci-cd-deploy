f761513f63bdf9e3967dfa2814d6f878ef98071e
Julio Duarte <julio.duarte@frgnet.com.br>
2023-11-03 17:18:23 -0300

    feat: implementação de count

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { useState } from "react";
+
 function App() {
-  return <div>DEPLOY FEITO</div>;
+  const [number, setNumber] = useState(0);
+  return (
+    <main>
+      <span>Counter {number}</span>
+      <button onClick={() => setNumber((n) => n + 1)}>+1</button>
+    </main>
+  );
 }
 
 export default App;
